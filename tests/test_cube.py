d4ca512437f038cae9457cced19a4d708927c688
marco.kronenberg@bafu.admin.ch <kronenberg.marco@gmail.com>
2025-02-10 20:13:34 +0100

    started work on hierarchies: implemented tests

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -166,3 +166,23 @@ class TestClass:
     def test_validate_whole(self):
         result_bool, result_message = self.cube.validate()
         assert result_message == "Cube is valid."
+
+    def test_hierarchies(self):
+        sparql = (
+            "ASK"
+            "{"
+            "  ?shape a cube:Constraint ;"
+            "    sh:property ?prop ."
+            "  ?prop sh:path mock:station ;"
+            "    meta:inHierarchy ?hierarchy ."
+            "  ?hierarchy a meta:Hierarchy ;"
+            "    meta:hierarchyRoot <https://mock.ld.admin.ch/station/switzerland> ;"
+            "    schema:name 'Schweiz' ;"
+            "    meta:nextInHierarchy ?nextInHierarchy ."
+            "  ?nextInHierarchy schema:name 'Stationen' ;"
+            "    sh:path schema:hasPart ;"
+            "}"
+        )
+
+        result = self.cube._graph.query(sparql)
+        assert bool(result)
