6a7133cf4bc0d51f006bc2d67d9c0f9814d9fd71
Rdataflow <waldgis@bafu.admin.ch>
2025-04-10 16:38:53 +0200

    add option to clear_graph
    helps for https://github.com/Kronmar-Bafu/lindas-pylindas/issues/49

--- pylindas/lindas/upload.py
+++ pylindas/lindas/upload.py
@@ -27,11 +27,13 @@ def _load_config(db_file:str, environment: str) -> dict:
 
     return config
 
-def upload_ttl(filename: str, db_file: str, environment: str, graph_uri: str):
+def upload_ttl(filename: str, db_file: str, environment: str, graph_uri: str, clear_graph: bool = False):
     conn_details = _load_config(db_file, environment)
 
     # todo: could graph_uri be specified in lindas.ini?
     with stardog.Connection("lindas", **conn_details) as conn:
         conn.begin()
+        if clear_graph:
+            conn.clear(graph_uri=graph_uri)
         conn.add(stardog.content.File(file=filename), graph_uri=graph_uri)
         conn.commit()
