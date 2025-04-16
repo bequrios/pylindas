9ac262c023776cca74000579e4325b48c7ca7d6f
JHirt-BAFU <joshua.hirt@bafu.admin.ch>
2025-04-16 17:23:32 +0200

    first test upload for loop

--- pylindas/lindas/upload.py
+++ pylindas/lindas/upload.py
@@ -1,6 +1,7 @@
 import requests
 import stardog
 from configparser import ConfigParser
+from typing import Union
 
 
 
@@ -27,13 +28,18 @@ def _load_config(db_file:str, environment: str) -> dict:
 
     return config
 
-def upload_ttl(filename: str, db_file: str, environment: str, graph_uri: str, clear_graph: bool = False):
+def upload_ttl(filename: Union[str,list], db_file: str, environment: str, graph_uri: str, clear_graph: bool = False):
     conn_details = _load_config(db_file, environment)
 
     # todo: could graph_uri be specified in lindas.ini?
     with stardog.Connection("lindas", **conn_details) as conn:
         conn.begin()
-        if clear_graph:
+        if clear_graph and graph_uri:
+            #if graph_URI is Null or not given as an arugment, conn.clear clears the whole database, we are not risking that.
             conn.clear(graph_uri=graph_uri)
-        conn.add(stardog.content.File(file=filename), graph_uri=graph_uri)
+        if isinstance(filename, str):
+            conn.add(stardog.content.File(file=filename), graph_uri=graph_uri)
+        else:
+            for files in filename:
+                conn.add(stardog.content.File(file=str(files)), graph_uri=graph_uri)
         conn.commit()
